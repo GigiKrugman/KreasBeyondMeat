aea38e8038dbe5cff119ca174a76c39136619f01
Marcello Mulas <marcellomulas93@gmail.com>
2023-03-13 10:01:23 +0100

    Initial Commit

---
+++ KreasBeyondMeat/KreasBeyondMeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  KreasBeyondMeatApp.swift
+//  KreasBeyondMeat
+//
+//  Created by Marcello Mulas on 13.03.23.
+//
+
+import SwiftUI
+
+@main
+struct KreasBeyondMeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
