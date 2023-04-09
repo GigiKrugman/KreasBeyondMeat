832c543582aa730b5ca156095bdb9791faca9f75
Marcello Mulas <marcellomulas93@gmail.com>
2023-04-09 21:38:28 +0200

    "New Functions added"

--- KreasBeyondMeat/ProductDetailView.swift
+++ KreasBeyondMeat/ProductDetailView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ProductDetailView: View {
+    @State var isFavorite: Bool = false
+    @State var number = 0
     let product: Product
     var body: some View {
         VStack {
@@ -19,9 +21,43 @@ struct ProductDetailView: View {
             Image(product.image)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-            
-            Text(product.description)
-                .font(.system(.title2, design: .monospaced))
+            VStack {
+                Text(product.description)
+                Text("Price: \(product.price) euro")
+            }
+            .font(.system(.title2, design: .monospaced))
+            HStack {
+                Button(action: { number = max(number + 1, 0)}) {
+                    Text("+")
+                        .font(.title)
+                        .foregroundColor(.green)
+                        .fontWeight(.heavy)
+                }
+                
+                Button(action: {number = max(number - 1, 0)}) {
+                  Text("\(number)")
+                    Text("-")
+                        .font(.title)
+                        .foregroundColor(.green)
+                        .fontWeight(.heavy)
+                }
+                
+                Spacer()
+                Button(action: {isFavorite.toggle()}) {
+                    Image(systemName: isFavorite ? "heart.fill" : "heart")
+                        .font(.title)
+                                                .foregroundColor(isFavorite ? .red : .gray)
+                }
+            }
+                                       HStack {
+                Button(action: {}) {
+                    Text("Add to chart")
+                        .font(.title)
+                        .padding(.all)
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                }
+            }
             Spacer()
         }
     }
